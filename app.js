b4e0f95cb8cd506d35fcf084232f0f6e88f54545
gonsaje <song.jaehyun@gmail.com>
2020-08-15 19:08:00 -0400

    enabled cors

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 app.get("/", (req, res) => res.send("Kitabu"));
 
