7130c9658fe742a2b2a402d0931dc3d2c1c96091
gonsaje <song.jaehyun@gmail.com>
2020-08-15 02:25:49 -0400

    prepare for deployment

--- app.js
+++ app.js
@@ -4,26 +4,34 @@ const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-
+const path = require('path');
 const users = require("./routes/api/users");
 const books = require("./routes/api/books");
+const collections = require("./routes/api/collections");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 app.get("/", (req, res) => res.send("Kitabu"));
 
 app.use(passport.initialize());
-// require('./passport/passport')(passport);
+require('./passport/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/api/users", users);
 app.use("/api/books", books);
-// app.use("/api/collections", collections);
+app.use("/api/collections", collections);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
