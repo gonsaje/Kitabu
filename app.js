ca41a102df12924d6da91ec9f90a9728fdf67385
gonsaje <song.jaehyun@gmail.com>
2020-08-15 19:28:19 -0400

    cors debug

--- app.js
+++ app.js
@@ -17,14 +17,6 @@ mongoose
 
 app.use(cors())
 
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
-
 app.get("/", (req, res) => res.send("Kitabu"));
 
 app.use(passport.initialize());
