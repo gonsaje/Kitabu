7673284f654cea01798b291b1444950ee9882252
gonsaje <song.jaehyun@gmail.com>
2020-08-15 02:27:54 -0400

    test

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('frontend/build'));
+//   app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//   })
+// }
 
 app.get("/", (req, res) => res.send("Kitabu"));
 
